0b465d72bcb73e2d6eb3109535712e72ebcb34d4
yiggalow <danny.kroll@gmx.de>
2021-05-07 14:00:40 +0200

    009: Implemented new method for get total change

--- Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/de/dhbw/Customer.java
+++ Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/de/dhbw/Customer.java
@@ -31,11 +31,19 @@ class Customer {
             frequentRenterPoints += each.getFrequentRenterPoints();
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return result;
+    }
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = this.rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
         return result;
     }
 
