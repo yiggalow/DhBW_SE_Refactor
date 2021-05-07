4233f4c7dea753b3fbaae1be0d1d6fd8aca39d75
yiggalow <danny.kroll@gmx.de>
2021-05-07 14:27:38 +0200

    014: added html statement to costumer

--- Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/de/dhbw/CustomerTest.java
+++ Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/de/dhbw/CustomerTest.java
@@ -6,42 +6,59 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerTest {
-    final String CUSTOMERNAME = "CustomerName";
-    final String MOVIETITEL = "I am an Movie";
+    final String CUSTOMERNAME = "Sasuke Uchiha";
+    final String MOVIETITLE = "Back To The Future";
+    final String MOVIETITLE2 = "Back To The Future 2";
     final int PRICECODE = 2;
-    final int RENTINGDAYS = 15;
-
-    final String RESULTSTRING = "de.dhbw.Rental Record for "+CUSTOMERNAME+"\n" +
-            "\tTitle\t\tDays\tAmount\n" +
-            "\t"+MOVIETITEL+"\t\t"+RENTINGDAYS+"\t19.5\n" +
-            "Amount owed is 19.5\n"+
-            "You earned 1 frequent renter points";
-
-    Movie movieForTesting;
-    Rental rentalForTesting;
-    Customer customerForTesting;
-
+    final int PRICECODE2 = 0;
+    final int RENTALDAYS = 69;
+    final int RENTALDAYS2 = 420;
+    Movie movie;
+    Movie movie2;
+    Rental rental;
+    Rental rental2;
+    Customer customer;
     @BeforeEach
     void setUp() {
-        customerForTesting = new Customer(CUSTOMERNAME);
-        movieForTesting = new Movie(MOVIETITEL,PRICECODE);
-        rentalForTesting = new Rental(movieForTesting,RENTINGDAYS);
+        movie = new Movie(MOVIETITLE, PRICECODE);
+        movie2 = new Movie(MOVIETITLE2, PRICECODE2);
+        rental = new Rental(movie, RENTALDAYS);
+        rental2 = new Rental(movie2, RENTALDAYS2);
+        customer = new Customer(CUSTOMERNAME);
     }
-
     @Test
     void addRental() {
-        customerForTesting.addRental(rentalForTesting);
+        customer.addRental(rental);
     }
 
     @Test
     void getName() {
-        assertEquals(CUSTOMERNAME,customerForTesting.getName());
+        assertEquals(CUSTOMERNAME, customer.getName(), "Expected the customer name to be " + CUSTOMERNAME);
+    }
+    @Test
+    void statement() {
+        customer.addRental(rental);
+        customer.addRental(rental2);
+        String correctResult = "Rental Record for " + CUSTOMERNAME + "\n";
+        correctResult += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+        correctResult += "\t" + MOVIETITLE + "\t" + "\t" + RENTALDAYS + "\t" + 100.5 + "\n";
+        correctResult += "\t" + MOVIETITLE2 + "\t" + "\t" + RENTALDAYS2 + "\t" + 629.0 + "\n";
+        correctResult += "Amount owed is " + 729.5 + "\n";
+        correctResult += "You earned " + 2 + " frequent renter points";
+
+        assertEquals(correctResult, customer.statement());
     }
 
     @Test
-    void statement() {
+    void htmlStatement() {
+        customer.addRental(rental);
+        customer.addRental(rental2);
+        String correctResult = "<h1>Rentals for <em>" + CUSTOMERNAME + "</em></h1><p>\n";
+        correctResult += MOVIETITLE + ": " + 100.5 + "<br>\n";
+        correctResult += MOVIETITLE2 + ": " + 629.0 + "<br>\n";
+        correctResult += "</p>You owe <em>" + 729.5 + "</em><p>\n";
+        correctResult += "On this rental you earned <em>" + 2 + "</em> frequent renter points</p>";
 
-        customerForTesting.addRental(rentalForTesting);
-        assertEquals(RESULTSTRING,customerForTesting.statement());
+        assertEquals(correctResult, customer.htmlStatement());
     }
 }
