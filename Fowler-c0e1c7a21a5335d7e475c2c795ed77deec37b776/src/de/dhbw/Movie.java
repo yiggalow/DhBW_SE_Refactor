2e99b1ade725e95f9086d28d05e345aad63feea1
yiggalow <danny.kroll@gmx.de>
2021-05-07 14:09:32 +0200

    012: Moving method and added submethod

--- Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/de/dhbw/Movie.java
+++ Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/de/dhbw/Movie.java
@@ -36,5 +36,8 @@ public class Movie {
         }
         return rentalAmount;
     }
-
+    int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) return 2;
+        else return 1;
+    }
 }
